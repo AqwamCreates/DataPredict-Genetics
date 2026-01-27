2343b06df2d715800d96f3fd3a1ea8626c77302b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-01-27 22:22:05 +0800

    Create Chromosome.lua

---
+++ src/Chromosome.lua
@@ -0,0 +1,43 @@
+local mathRandom = math.random
+
+local Chromosome = {}
+
+function Chromosome.new(parameterDictionary)
+
+	parameterDictionary = parameterDictionary or {}
+
+	local self = setmetatable({}, Chromosome)
+
+	local geneArray = parameterDictionary.geneArray or parameterDictionary[1] or {}
+	
+	local activationFunction = parameterDictionary.activationFunction or parameterDictionary[2] or function(...) return ... end
+	
+	local mutationChance = parameterDictionary.mutationChance or parameterDictionary[3] or 0
+
+	return self
+
+end
+
+function Chromosome:mutate(forceMutateChromosome, forceMutateGene)
+	
+	if (not forceMutateChromosome) and (self.mutationChance <= mathRandom()) then return end
+	
+	for i, Gene in ipairs(self.geneArray) do Gene:mutate(forceMutateGene) end
+	
+end
+
+function Chromosome:activate()
+	
+	local valueArray = {}
+	
+	for i, gene in ipairs(self.geneArray) do
+		
+		valueArray[i] = gene.value
+		
+	end
+	
+	return self.activationFunction(table.unpack(valueArray))
+	
+end
+	
+return Chromosome
